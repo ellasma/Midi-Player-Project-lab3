a55ff884b7c0ab2d891041a928140f42e48648d5
Asma Ellahe <ellasma078@gmail.com>
2025-09-12 22:05:01 -0400

    step 48

---
+++ TuneCreator.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class TuneCreator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TuneCreator
+{
+    // instance variables - replace the example below with your own
+    private MidiPlayer player;
+
+    /**
+     * Constructor for objects of class TuneCreator
+     */
+    public TuneCreator()
+    {
+        // initialise instance variables
+        player= new MidiPlayer();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void CreateAndPlay()
+    {
+        // put your code here
+        
+    }
+}
